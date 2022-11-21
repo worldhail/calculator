3fb990cf69c8730a2767ad6d6cc53499241627e0
Jeric Chua <worldhail41@gmail.com>
2022-11-21 22:59:03 +0800

    Disallowing operator to display as first element

--- app.js
+++ app.js
@@ -1,29 +1,28 @@
+//Note: I use guard clauses on some of the if else statement for oneliner code
+
 const calculatorScreen = document.querySelector('.input-value');
 const toBeEqualledScreen = document.querySelector('.to-be-equalled');
 const operator = /[\+]|[\-]|[\*]|[\/]/;
-const operatorWithFlag = /[\+]|[\-]|[\*]|[\/]/g;
-let forDisplay = [];
+//const negativeSign = 
 const unFinishedTotal = [''];
 let lastUsedOperator = '';
+let forDisplay = [];
 
 //KEYPRESS EVENT LISTENER
 window.addEventListener('keypress', function (event) {
     const number = /[\d]|[\.]/;
     const pressedKey = event.key;
-    const lastUsedOperatorLength = lastUsedOperator.length;
     const unFinishedTotalLastElement = unFinishedTotal.slice(-1)[0];
 
     if (number.test(pressedKey)) {
-        if (unFinishedTotalLastElement.match(operator)) {
-            forDisplay = [];
-        }
-        if (forDisplay.includes('.') && pressedKey === '.') { return }
+        if (unFinishedTotalLastElement.match(operator)) { forDisplay = []; }
+        if (forDisplay.includes('.') && pressedKey === '.') { return; }
         display(pressedKey);
 
     } else if (operator.test(pressedKey)) {
-        if (unFinishedTotalLastElement.match(operator)) {
-            unFinishedTotal.pop();
-        } else if (lastUsedOperator !== '' && unFinishedTotalLastElement.match(number)) {
+        if (unFinishedTotalLastElement.match(operator)) { unFinishedTotal.pop() }
+        if (forDisplay.length === 0) { return; }
+        else {
             const total = calculate(unFinishedTotal);
             forDisplay = [];
             display(total);
@@ -36,8 +35,8 @@ window.addEventListener('keypress', function (event) {
     const starAndSlash = { '*': 'x', '/': '÷' };
     const pressedKeys = unFinishedTotal
         .join('')
-        .replace(/[\*]|[\/]/g, (match) => {
-            return starAndSlash[match]
+        .replace(/[\*]|[\/]/g, (values) => {
+            return starAndSlash[values]
         });
     toBeEqualledScreen.value = pressedKeys;
 });
@@ -49,7 +48,7 @@ function display(number) {
 
     if (number === '.' && forDisplayLength === 0) {
         forDisplay.push(0);
-
+        unFinishedTotal.push(0);
     } else if (forDisplayFirstIndex === '0' && forDisplayLength === 1) {
         if (number !== '.') {
             forDisplay.pop();
